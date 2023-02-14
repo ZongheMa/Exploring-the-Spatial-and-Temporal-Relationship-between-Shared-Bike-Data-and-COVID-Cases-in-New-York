02970789860560f9205fbc14a46499cb5e418e8f
Elio-Zonghe <elioma@163.com>
2023-02-14 16:25:57 +0000

    NYC_BikeData_Cleaned

--- data_clean.py
+++ data_clean.py
@@ -1,15 +1,15 @@
 # coding=utf-8
-import matplotlib.pyplot as plt
 import numpy as np
+import glob
 import pandas as pd
 import os, sys
 import zipfile
 import geopandas as gpd
 from shapely.geometry import MultiPoint, Point
 import warnings
-import matplotlib.pyplot as plt
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 '''# Covid-19 data from NYC Health
 original_covid_nyc = pd.read_csv('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/trends/data-by-day.csv')
@@ -27,15 +27,17 @@ pd.plotting.scatter_matrix(covid_nyc, alpha=0.2, figsize=(6, 6), diagonal='kde')
 plt.show()'''
 
 
-# class NYC_bikeData_Clean(self):
-#     self.source_folder = s
-#     self.target_folder = '/Users/zonghe/Documents/Modules/Term2/CEGE0042_STDM/STDM/data/shared bike datasets/nyc'
-#
+def get_csv_paths(folder_path):
+    csv_files = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".csv"):
+                file_path = os.path.join(root, file)
+                csv_files.append(file_path)
+    return csv_files
 
 
 # Shared bike datasets
-
-
 def unzip_files(source_folder, target_folder):
     for filename in os.listdir(source_folder):
         if filename.endswith('.zip'):
@@ -44,96 +46,164 @@ def unzip_files(source_folder, target_folder):
                 zip_ref.extractall(target_folder)
 
 
-source_folder = '/Users/zonghe/Downloads/'
-target_folder = '/Users/zonghe/Documents/Modules/Term2/CEGE0042_STDM/STDM/data/shared bike datasets/nyc'
-# unzip_files(source_folder, target_folder)
+def merge_csv(source_folder, target_folder):
+    all_files = glob.glob(source_folder + "/*.csv")
+    list_ = []
+    for file in all_files:
+        df = pd.read_csv(file, index_col=None, header=0)
+        list_.append(df)
+
+    frame = pd.concat(list_, axis=0, ignore_index=True)
+    frame.to_csv(
+        target_folder + 'merged.csv',
+        index=False)
 
 
-# # Merge csv files
-# all_files = glob.glob(target_folder + "/*.csv")
-# list_ = []
-# for file in all_files:
-#     df = pd.read_csv(file, index_col=None, header=0)
-#     list_.append(df)
-#
-# frame = pd.concat(list_, axis=0, ignore_index=True)
-# frame.to_csv('/Users/zonghe/Documents/Modules/Term2/CEGE0042_STDM/STDM/data/shared bike datasets/nyc_sharedbikes_merged.csv', index=False)
+class NYUbike_Clean:
+    def __init__(self, csv):
+        self.ori_csv = csv
 
+    def uniform(self):
+        df = pd.read_csv(self.ori_csv)
+        if 'tripduration' not in df.columns:
+            df.rename(
+                columns={'start_station_id': 'start station id', 'started_at': 'starttime', 'ended_at': 'stoptime',
+                         'start_station_name': 'start station name', 'start_lat': 'start station latitude',
+                         'start_lng': 'start station longitude'}, inplace=True)
+            df['tripduration'] = (pd.to_datetime(df['stoptime']) - pd.to_datetime(df['starttime'])).dt.seconds
+            df['tripduration'] = df['tripduration'].astype(float)
+            df.to_csv(self.ori_csv, index=False)
 
-ori_csv = pd.read_csv('data/shared bike datasets/nyc/201910-citibike-tripdata.csv')
-ori_len = len(ori_csv)
-ori_csv[(ori_csv['tripduration'] > 60) & (ori_csv['tripduration'] < 21600)]
-print(f'Filter out {len(ori_csv) - ori_len} invalid data（out of time data)')
+    def basic_csv(self):
+        df = pd.read_csv(self.ori_csv,low_memory=False)
 
-ori_csv['starttime'], ori_csv['stoptime'] = pd.to_datetime(ori_csv['starttime']), pd.to_datetime(ori_csv['stoptime'])
-# print(type(a['starttime'][1]))
-ori_csv['starttime'], ori_csv['stoptime'] = ori_csv['starttime'].dt.strftime('%Y-%m-%d'), ori_csv[
-    'stoptime'].dt.strftime('%Y-%m-%d')
-# print(ori_csv['starttime'][:])
+        # filter out invalid data
+        ori_len = len(df)
+        df = df[(df['tripduration'] > 60) & (df['tripduration'] < 21600)]
+        print(f'Filtered out {ori_len - len(df)} invalid data (out of time range data).')
 
-csv = pd.DataFrame()
+        # convert to datetime and extract date
+        df['starttime'] = pd.to_datetime(df['starttime'])
+        df['stoptime'] = pd.to_datetime(df['stoptime'])
+        df['starttime'] = df['starttime'].dt.date.astype(str)
+        df['stoptime'] = df['stoptime'].dt.date.astype(str)
 
-csv['date'] = ori_csv.groupby(['starttime']).agg({'starttime': 'first'})
-csv['tripcount'] = ori_csv.groupby(['starttime']).agg({'tripduration': 'count'})
-csv['tripdurtion_sum'] = ori_csv.groupby(['starttime']).agg({'tripduration': 'sum'})
-csv['tripcount_mean'] = ori_csv.groupby(['starttime']).agg({'tripduration': 'mean'})
-csv['active_stations_id'] = ori_csv.groupby(['starttime'])['start station id'].agg(lambda x: x.unique().tolist())
-csv['active_stations_name'] = ori_csv.groupby(['starttime'])['start station name'].agg(lambda x: x.unique().tolist())
+        # group and aggregate
+        group_cols = ['starttime', 'start station id', 'start station name',
+                      'start station latitude', 'start station longitude']
+        agg_dict = {'tripduration': ['count','sum', 'mean'], 'start station name': 'first'}
+        grouped = df.groupby(group_cols).agg(agg_dict).reset_index()
+        grouped.columns = ['_'.join(col).strip() for col in grouped.columns.values]
+        grouped.rename(columns={'starttime_': 'starttime',
+                                'start station id_': 'start station id',
+                                'start station name_': 'start station name',
+                                'start station latitude_': 'start station latitude',
+                                'start station longitude_': 'start station longitude',
+                                'tripduration_count': 'trip count',
+                                'tripduration_sum': 'tripduration_sum(sec)',
+                                'tripduration_mean': 'tripduration_mean(sec)'}, inplace=True)
+        grouped.drop(columns=['start station name_first'], inplace=True)
 
-unique_stations = ori_csv[['start station id']].drop_duplicates().reset_index(drop=True).sort_values(
-    by='start station id')
-# print(unique_stations)
-sta_info = ori_csv.loc[:,
-           ['start station id', 'start station name', 'start station latitude', 'start station longitude']]
-# print(sta_info.columns)
-merge_station = unique_stations.merge(sta_info, on='start station id', how='left').drop_duplicates().reset_index(
-    drop=True)
-merge_station['active_station_count'] = ori_csv.groupby(['starttime'])['start station id'].count()
-# print(merge_station.head())
+        # convert units and create geometry column
+        grouped['tripduration_sum(mins)'] = round(grouped['tripduration_sum(sec)'] / 60, 4)
+        grouped['tripduration_mean(mins)'] = round(grouped['tripduration_mean(sec)'] / 60, 4)
+        grouped['geometry'] = [Point(xy) for xy in zip(grouped['start station longitude'],
+                                                       grouped['start station latitude'])]
+        gdf = gpd.GeoDataFrame(grouped, geometry='geometry', crs='epsg:4326')
 
+        return gdf
 
+    def complex_csv(self):
 
-df = pd.DataFrame()
-active_station_count = []
-for date in ori_csv['starttime'].unique():
-    rows = ori_csv[ori_csv['starttime'] == date]
-    count = 0
-    for station in rows['start station id'].unique():
-        count = rows[rows['start station id'] == station]['start station id'].count()
-        active_station_count.append(count)
-    df = df.append({'date': date, 'active_stations_id': rows['start station id'].unique(),
-                    'active_stations_name': rows['start station name'].unique(),
-                    'active_stations_count': active_station_count,
-                    'lon': merge_station['start station longitude'], 'lat': merge_station['start station latitude'],
-                    'tripcount': len(rows), 'tripdurtion_sum(mins)': rows['tripduration'].sum() / 60,
-                    'tripdurtion_mean(mins)': rows['tripduration'].mean() / 60}, ignore_index=True)
+        ori_csv = pd.read_csv(self.ori_csv)
+        ori_len = len(ori_csv)
+        ori_csv = ori_csv[(ori_csv['tripduration'] > 60) & (ori_csv['tripduration'] < 21600)]
+        print(f'Filter out {len(ori_csv) - ori_len} invalid data（out of time data)')
 
-dic_list1 = []
-dic_list2 = []
-points_list = []
-multipoints_list = []
-for i in range(len(df)):
-    for j in range(len(df['active_stations_id'][i])):
-        dic = {'station_id': df['active_stations_id'][i][j],
-               'station_name': df['active_stations_name'][i][j],
-               'point': Point(df['lon'][i][j], df['lat'][i][j])
-               }
-        dic_list2.append(dic)
+        csv = pd.DataFrame()
+        csv['date'] = ori_csv.groupby(['starttime']).agg({'starttime': 'first'})
+        csv['tripcount'] = ori_csv.groupby(['starttime']).agg({'tripduration': 'count'})
+        csv['tripdurtion_sum'] = ori_csv.groupby(['starttime']).agg({'tripduration': 'sum'})
+        csv['tripcount_mean'] = ori_csv.groupby(['starttime']).agg({'tripduration': 'mean'})
+        csv['active_stations_id'] = ori_csv.groupby(['starttime'])['start station id'].agg(
+            lambda x: x.unique().tolist())
+        csv['active_stations_name'] = ori_csv.groupby(['starttime'])['start station name'].agg(
+            lambda x: x.unique().tolist())
 
-    dic_list1.append(dic_list2)
+        unique_stations = ori_csv[['start station id']].drop_duplicates().reset_index(drop=True).sort_values(
+            by='start station id')
+        # print(unique_stations)
+        sta_info = ori_csv.loc[:,
+                   ['start station id', 'start station name', 'start station latitude', 'start station longitude']]
+        # print(sta_info.columns)
+        merge_station = unique_stations.merge(sta_info, on='start station id',
+                                              how='left').drop_duplicates().reset_index(
+            drop=True)
+        merge_station['active_station_count'] = ori_csv.groupby(['starttime'])['start station id'].count()
+        # print(merge_station.head())
 
-for i in range(len(dic_list1)):
-    for j in range(len(dic_list1[i])):
-        points_list.append(dic_list1[i][j]['point'])
-    geometry = MultiPoint(points_list)
-    multipoints_list.append(geometry)
+        df = pd.DataFrame()
+        active_station_count = []
+        for date in ori_csv['starttime'].unique():
+            rows = ori_csv[ori_csv['starttime'] == date]
+            count = 0
+            for station in rows['start station id'].unique():
+                count = rows[rows['start station id'] == station]['start station id'].count()
+                active_station_count.append(count)
+            df = df.append({'date': date, 'active_stations_id': rows['start station id'].unique(),
+                            'active_stations_name': rows['start station name'].unique(),
+                            'active_stations_count': active_station_count,
+                            'lon': merge_station['start station longitude'],
+                            'lat': merge_station['start station latitude'],
+                            'tripcount': len(rows), 'tripdurtion_sum(mins)': rows['tripduration'].sum() / 60,
+                            'tripdurtion_mean(mins)': rows['tripduration'].mean() / 60}, ignore_index=True)
 
-df['geometry'] = multipoints_list
-s_df = gpd.GeoDataFrame(df, geometry='geometry', crs={'init': 'epsg:4326'})
+        t_df = pd.DataFrame()
+        active_station_count = []
+        for date in ori_csv['starttime'].unique():
+            rows = ori_csv[ori_csv['starttime'] == date]
+            count = 0
+            for station in rows['start station id'].unique():
+                count = rows[rows['start station id'] == station]['start station id'].count()
+                active_station_count.append(count)
+            t_df = df.append({'date': date, 'active_stations_id': rows['start station id'].unique(),
+                              'active_stations_name': rows['start station name'].unique(),
+                              'active_stations_count': active_station_count,
+                              'lon': merge_station['start station longitude'],
+                              'lat': merge_station['start station latitude'],
+                              'tripcount': len(rows), 'tripdurtion_sum(mins)': rows['tripduration'].sum() / 60,
+                              'tripdurtion_mean(mins)': rows['tripduration'].mean() / 60}, ignore_index=True)
 
-t_df = s_df.explode('date').explode('active_stations_id').explode('active_stations_name').explode('active_stations_count').explode('lon').explode('lat').explode('tripcount').explode('tripdurtion_sum(mins)').explode('tripdurtion_mean(mins)')
-t_df.reset_index(drop=True, inplace=True)
+        dic_list1 = []
+        dic_list2 = []
+        points_list = []
+        multipoints_list = []
+        for i in range(len(df)):
+            for j in range(len(df['active_stations_id'][i])):
+                dic = {'station_id': df['active_stations_id'][i][j],
+                       'station_name': df['active_stations_name'][i][j],
+                       'point': Point(df['lon'][i][j], df['lat'][i][j])
+                       }
+                dic_list2.append(dic)
 
-# s_df.drop(columns=['lon', 'lat', 'active_stations_id', 'active_stations_name', 'active_stations_count'], inplace=True)
-print(s_df.head())
-print(t_df.head())
+            dic_list1.append(dic_list2)
+
+        for i in range(len(dic_list1)):
+            for j in range(len(dic_list1[i])):
+                points_list.append(dic_list1[i][j]['point'])
+            geometry = MultiPoint(points_list)
+            multipoints_list.append(geometry)
+
+        df['geometry'] = multipoints_list
+        t_df = gpd.GeoDataFrame(df, geometry='geometry', crs={'init': 'epsg:4326'})
+
+        return t_df
+
+    def complex_drop(self):
+
+        t_df = self.complex_csv()
+
+        t_df.drop(['active_stations_id', 'active_stations_name', 'active_stations_count', 'lon', 'lat'], axis=1,
+                  inplace=True)
+
+        return t_df
