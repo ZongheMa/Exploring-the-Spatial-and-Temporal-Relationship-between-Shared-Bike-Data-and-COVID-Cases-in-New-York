fbf6d0e701ebc511502efe9daeb7813559851594
Elio-Zonghe <elioma@163.com>
2023-03-07 18:17:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from data_clean import get_csv_paths, unzip_files, merge_csv, BikeDataClean
+from data_clean import *
 # # Unzip raw files
 # source_folder = '/Users/zonghe/Downloads/'
 # target_folder = '/Users/zonghe/Documents/Modules/Term2/CEGE0042_STDM/STDM/data/SharedBike/ori_bst'
@@ -27,18 +27,18 @@ from data_clean import get_csv_paths, unzip_files, merge_csv, BikeDataClean
 # covid_nyc = covid_nyc.loc[covid_nyc['date_of_interest'] < '2023-02-01']
 # covid_nyc.rename(columns={'date_of_interest': 'date'}, inplace=True)
 #
-# db = pd.DataFrame(pd.date_range(start='2019-10-01', end='2020-02-28', freq='D').astype(str), columns=['date'])
+# db = pd.DataFrame(pd.date_range(start='2019-01-01', end='2020-02-28', freq='D').astype(str), columns=['date'])
 #
 # covid_nyc = pd.concat([db, covid_nyc], axis=0, join='outer', ignore_index=True).fillna(0)
 # covid_nyc.to_csv('data/Covid_cases/covid_nyc.csv', index=False)
-
-
+#
+#
 # # Covid cases data from City of BST(https://www.boston.gov/government/cabinets/boston-public-health-commission/covid-19-boston )
 # df = pd.read_csv('data/Covid_cases/Boston_COVID-19_NewCases.csv')
 # df = df.loc[:, ['Category1', 'Value']]
 # df.rename(columns={'Category1': 'date', 'Value': 'cases'}, inplace=True)
 # df['date'] = pd.to_datetime(df['date'], format='%m/%d/%Y').astype(str)
-# db = pd.DataFrame(pd.date_range(start='2019-10-01', end='2020-02-21', freq='D').astype(str), columns=['date'])
+# db = pd.DataFrame(pd.date_range(start='2019-01-01', end='2020-02-21', freq='D').astype(str), columns=['date'])
 # df = pd.concat([db, df], axis=0, join='outer', ignore_index=True).fillna(0)
 # df.index = pd.to_datetime(df['date'])
 # df.resample('D').mean().interpolate() # interpolate the missing values
